12fa30ccd1b0be2b7f6850100dd7ef7220063d71
Rafael Varela <rafaelvrcr@gmail.com>
2021-06-11 09:44:27 -0300

    Adapter configurado para teste 1

--- app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cardview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,39 +9,48 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cardview.R;
+
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.postagem_detalhe,parent,false);
 
-
-        return null;
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        holder.textNome.setText("Rafael Varela");
+        holder.textPostagem.setText("Me contrata");
+        holder.imagePostagem.setImageResource(R.drawable.imagem1);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imagePostagem;
 
-        public MyViewHolder(@NonNull View itemView, TextView nome, TextView postagem, ImageView imagem) {
+        public MyViewHolder(View itemView){
             super(itemView);
-            this.nome = nome;
-            this.postagem = postagem;
-            this.imagem = imagem;
+        textNome = itemView.findViewById(R.id.textNome);
+        textPostagem = itemView.findViewById(R.id.textPostagem);
+        imagePostagem = itemView.findViewById(R.id.imagePostagem);
+                }
+
+            }
         }
     }
-    }
 
-}
+
